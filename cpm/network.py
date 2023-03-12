5c3012c887b27cf4bd3df5309e49abc2cd81a445
Nielotz <mati2806@poczta.fm>
2023-03-13 00:04:37 +0100

    Add partial solve: traverse_to_right
    Remove unnecessary constructor arguments; change default fields value to None;

--- cpm/network.py
+++ cpm/network.py
@@ -1,24 +1,23 @@
 import copy
-from typing import Self
+from typing import Any
 
 
 class Node:
-    def __init__(self, id_: str, prev_nodes: [Self, ], duration: float,
-                 early_start: float = 0, early_final: float = 0, late_start: float = 0, late_final: float = 0,
-                 possible_delay: float = 0):
+    # noinspection PyTypeChecker
+    def __init__(self, id_: str, prev_nodes: [Any, ], duration: float):
         """ A node in a CPM network. """
 
         """ Used to specify task to solve. """
         self.id_: str = id_  # Czynność
-        self.prev_nodes: [Self, ] = copy.deepcopy(prev_nodes)
+        self.prev_nodes: [Any, ] = copy.deepcopy(prev_nodes)
         self.duration: float = duration
 
         """ Calculated using Network.solve """
-        self.early_start: float = early_start
-        self.early_final: float = early_final
-        self.late_start: float = late_start
-        self.late_final: float = late_final
-        self.possible_delay: float = possible_delay
+        self.early_start: float = None
+        self.early_final: float = None
+        self.late_start: float = None
+        self.late_final: float = None
+        self.possible_delay: float = None
 
     def asdict(self):
         return {
@@ -35,19 +34,68 @@ class Node:
 class Network:
     """ Holds Nodes and calculates CPM method params. """
     nodes: [Node, ] = []
+    nodes_by_id: {} = dict()
     critical_paths: [[Node, ], ] = []
 
     def add_node(self, node: Node):
         self.nodes.append(node)
+        self.nodes_by_id[node.id_] = node
 
     def solve(self):
         """ Calculate missing nodes' params and critical path. """
 
+        # Traverse to the end: calculate: ES, EF
+        def traverse_to_right():
+            """
+            Traverse events from start to the end.
+
+            Fills early_start and early_final.
+
+            :return: list somehow sorted: from the last to the first. First item is always the final one.
+            """
+            nodes_to_parse: [int, ] = list(range(len(self.nodes)))[::-1]
+            parsed_nodes: [Node, ] = []
+            while len(nodes_to_parse) > 0:
+                for node_id in nodes_to_parse[::-1]:
+                    node: Node = self.nodes[node_id]
+                    if not node.prev_nodes:
+                        node.early_start = node.late_start = node.possible_delay = 0  # Start
+                        node.early_final = node.early_start + node.duration
+                    elif node.prev_nodes and None not in (
+                            prev_early_first := [self.nodes_by_id[prev_id].early_final for prev_id in node.prev_nodes]):
+                        node.early_start = max(prev_early_first)
+                        node.early_final = node.early_start + node.duration
+                    else:
+                        continue
+                    nodes_to_parse.remove(node_id)
+                    parsed_nodes.append(node)
+
+            return parsed_nodes
+
+        pre_sorted: [Node] = traverse_to_right()
+
+        # Traverse backward: calculate: LS, LF, delay
+        def traverse_backward(nodes: [Node]):
+            """
+            Traverse events from end to the start.
+
+            Fills late_start, late_final and possible_delay.
+            """
+
+        traverse_backward(pre_sorted)
+        # self.late_start: float = late_start
+        # self.late_final: float = late_final
+        # self.possible_delay: float = possible_delay
+
+        """ Fix orphan tasks. """
+        # Add apparent tasks that connect to the final one.
+
+        """ Get critical path, by selecting tasks with 0 possible delay. """
         # There can be multiple critical paths.
         # self.critical_paths = [[id1, id2...]]
         pass
 
     def __repr__(self):
         return f"Network:\n" \
-               f"\t Critical path:" + "\t\t".join(self.critical_paths) \
+               f"\t Critical path:" + "\t\t".join([str(cp) for cp in self.critical_paths]) \
             + f"\n\tNodes: \n\t\t" + '\n\t\t'.join([str(node) for node in self.nodes])
