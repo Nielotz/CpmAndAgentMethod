f2e0dfb1098ff0deebd7b40491431c9a01735fc1
Bartłomiej Gojowczyk <bartek.gojowczyk01@gmail.com>
2023-03-14 19:58:27 +0100

    Sth

--- main.py
+++ main.py
@@ -7,27 +7,28 @@ from kivy.core.window import Window
 
 class CPMapp(App):
     def build(self):
-        net: tuple[network.Network, network.Network] = dl.load_data_from_file()
+        net: tuple[network.Network, network.Network] = dl.load_data_from_file(path="cpm\\test_data\\test_two_critical_paths_that_ends_with_orphans.txt")
+        nn = net[0].solve()
 
         gra = graph.GraphWidget()
         # gra.set_network(net[0])
-        gra.draw_graph(net[0])
+        gra.draw_graph(nn[0])
         return gra
 
 Window.clearcolor = (218/255, 222/255, 206/255, 1.0)
 CPMapp().run()
 
-""" Create CPM sth. """
-brrr_system = cpm.CPM()
+# """ Create CPM sth. """
+# brrr_system = cpm.CPM()
 
-""" Load data from user (to load data from file, see cpm/test/data_loader.py). """
-# Input
-brrr_system.load_data_from_user ()
+# """ Load data from user (to load data from file, see cpm/test/data_loader.py). """
+# # Input
+# brrr_system.load_data_from_user ()
 
-""" Solve network. """
-# Brrrrr
-brrr_system.solve()
+# """ Solve network. """
+# # Brrrrr
+# brrr_system.solve()
 
-# Output
-brrr_system.print_result_network()
-# brrr_system.show_result_network()
+# # Output
+# brrr_system.print_result_network()
+# # brrr_system.show_result_network()
