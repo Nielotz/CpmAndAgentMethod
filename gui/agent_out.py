5c8adc6be5a6235a03a7c6c1dbf4baa9203a9826
Bartłomiej Gojowczyk <bartek.gojowczyk01@gmail.com>
2023-05-15 16:32:41 +0200

    Baisic gui output

---
+++ gui/agent_out.py
@@ -0,0 +1,188 @@
+from math import sin, cos, atan, pi, sqrt
+
+from kivy.graphics import Line, Ellipse, Color, Rectangle
+from kivy.uix.button import Button
+from kivy.uix.effectwidget import EffectWidget
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, NoTransition, Screen
+
+# To do
+# -> move labels
+# -> better colors
+
+class AgentData:
+    cost: float = 1111
+    income: float = 2222
+    profit: float = 223
+    profit_table: list[list[int]] = [[1,2,3],[4,5,6]]
+    optimal_transport_table: list[list[int]] = [[153,7,8],[9,0,1]]
+
+
+class ColorLabel(Label):
+    def __init__(self, bacground_color=(1., 0., 1., 0.25), **kwargs):
+        super().__init__(**kwargs)
+        self.bacground_color = bacground_color
+        self.gap = 2
+
+    def on_size(self, *args):
+        self.canvas.before.clear()
+        with self.canvas.before:
+            Color(*self.bacground_color)
+            Rectangle(pos=(self.pos[0]+self.gap, self.pos[1]+self.gap), 
+                      size=(self.size[0]-2*self.gap, self.size[1]-2*self.gap))
+
+
+class AgentWidget(Widget):
+    def __init__(self, agent_data: AgentData, **kwargs):
+        super().__init__(**kwargs)
+        self.agent_data = agent_data
+        
+        self.draw_summary()
+        self.draw_profit_table()
+        self.draw_optimal_transport_table()
+       
+    def draw_summary(self):
+        summary_size = (270, 50)
+        summary_pos = ((Window.size[0] - summary_size[0]) / 2, Window.size[1] - 2 * summary_size[1])
+        summary_padding = 5
+        label_size = (100,20)
+        text_color = (.1, .1, .1)
+        font_size = 18
+        
+
+        with self.canvas:
+            Color(.6, .6, .6, 0.3)
+            Rectangle(size=summary_size, pos=summary_pos)
+        
+
+        text='[b]Income: [color=#008000]' + str(self.agent_data.income) + '[/color][/b]'
+        income_label = Label(text=text, 
+                             color=text_color,
+                             size=label_size, 
+                             halign='left',
+                             size_hint=(None, None),
+                             font_size=font_size,
+                             markup=True)
+        income_label.pos = (summary_padding, label_size[1] + summary_padding)
+        income_label.bind(texture_size=income_label.setter('size'))
+
+
+        text='[b]Cost: [color=#800000]' + str(self.agent_data.cost) + '[/color][/b]'
+        cost_label = Label(text=text, 
+                             color=text_color,
+                             size=label_size, 
+                             halign='left',
+                             size_hint=(None, None),
+                             font_size=font_size,
+                             markup=True)
+        cost_label.pos = (summary_padding,0)
+        cost_label.bind(texture_size=cost_label.setter('size'))
+
+        
+        text='[b]Profit: [color=#00C000]'+str(self.agent_data.profit) + '[/color][/b]'
+        profit_label = Label(text=text, 
+                             color=text_color,
+                             halign='right',
+                             valign='center',
+                             size_hint=(None, None),
+                             font_size=font_size,
+                             markup=True)
+        profit_label.size = (profit_label.size[0], summary_size[1])
+        profit_label.text_size = profit_label.size
+        profit_label.pos = (summary_size[0] - profit_label.size[0]-summary_padding, 0)
+
+
+        layout = RelativeLayout(pos=summary_pos)
+        layout.add_widget(cost_label)
+        layout.add_widget(income_label)
+        layout.add_widget(profit_label)
+
+        self.add_widget(layout)
+
+    def draw_optimal_transport_table(self):
+        pos = (425, 100)
+        size = (350, 300)
+        
+        with self.canvas:
+            Color(0.3, 0.3, 0.3, 1.)
+            Rectangle(size=size, pos=pos)
+
+        layout = AnchorLayout(size=(size[0], 40), pos=(pos[0], pos[1] + size[1]))
+        layout.add_widget(ColorLabel(text="Optimal transport table", 
+                                                   bacground_color=(0.3, 0.3, 0.3,1.), 
+                                                   color=(1, 1, .55),
+                                                   font_size=18))
+        
+        self.add_widget(layout)
+        self.add_widget(self.generate_table(pos,size,self.agent_data.optimal_transport_table))
+
+    def draw_profit_table(self):
+        pos = (25, 100)
+        size = (350, 300)
+        
+        with self.canvas:
+            Color(0.3, 0.3, 0.3, 1.)
+            Rectangle(size=size, pos=pos)
+
+        layout = AnchorLayout(size=(size[0], 40), pos=(pos[0], pos[1] + size[1]))
+        layout.add_widget(ColorLabel(text="Profit table", 
+                                                   bacground_color=(0.3, 0.3, 0.3,1.), 
+                                                   color=(1, 1, .55),
+                                                   font_size=18))
+        
+        # profit_table_label = Label(text="Profit table", color=(0,1,.55), size=(size[0],20), font_size=18)
+        # profit_table_label.pos = (pos[0], pos[1] + size[1] - profit_table_label.size[1]-10)
+        
+        self.add_widget(layout)
+        self.add_widget(self.generate_table(pos,size,self.agent_data.profit_table))
+
+    def generate_table(self, pos, size, data:list[list[int]]):
+        background_color = (.95, .95, .95, 1.)
+        text_color = (.5, .88, .5)
+        font_size=15
+
+        layout = GridLayout(cols=len(data[0])+1, pos=pos, size=size)
+        layout.add_widget(ColorLabel(text=" ", bacground_color=(0,0,0,0), font_size=font_size))
+
+        for i in range(len(data[0])):
+            layout.add_widget(ColorLabel(text="[b]R"+str(i)+"[/b]",
+                                         markup=True,
+                                         color=text_color, 
+                                         bacground_color=background_color,
+                                         font_size=font_size))
+
+        for i in range(len(data)):
+            layout.add_widget(ColorLabel(text="[b]S"+str(i)+"[/b]",
+                                         markup=True, 
+                                         color=text_color, 
+                                         bacground_color=background_color,
+                                         font_size=font_size))
+            for j in data[i]:
+                layout.add_widget(ColorLabel(text=str(j), 
+                                             color=text_color, 
+                                             bacground_color=background_color,
+                                             font_size=font_size))
+
+        return layout
+
+
+class AgentManager(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        
+class TestAgentWidget(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+        data = AgentData()
+        self.add_widget(AgentWidget(agent_data=data))
+
+
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from kivy.app import App
 from kivy.core.window import Window
 
 from gui.screen_manager import *
-
+from gui.agent_out import TestAgentWidget
 
 class CPMapp(App):
     def __init__(self, **kwargs):
@@ -23,8 +23,8 @@ class CPMapp(App):
 
     def build(self):
         Window.clearcolor = (218 / 255, 222 / 255, 206 / 255, 1.0)
-
-        return MyScreenManager()
+        return TestAgentWidget()
+        # return MyScreenManager()
 
 
 CPMapp().run()
