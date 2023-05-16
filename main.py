9e9bb26bdb288d5cc444a633268c49e125860bac
Nielotz <mati2806@poczta.fm>
2023-05-16 20:42:53 +0200

    Clean main

--- main.py
+++ main.py
@@ -1,30 +1,13 @@
 from kivy.app import App
 from kivy.core.window import Window
 
-from gui.screen_manager import *
+from gui.screen_manager import MyScreenManager
 
-
-class CPMapp(App):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.result_networks: [Network, ] = None
-
-    @staticmethod
-    def load_data_from_user(path: str) -> {Hashable, Node}:
-        """ Load data from user.
-
-        @return: dict{Node.id_, Node} - dict of nodes with nodes' id as a key
-        """
-        nodes: {Hashable, Node} = dict()
-        read_data = data_input.load_data_from_file(path=path)
-        for id_, prev_ids, duration in read_data[tuple(read_data.keys())[0]]:
-            nodes[id_] = Node(id_, prev_ids.split(",") if prev_ids else [], float(duration))
-        return nodes
-
+class CpmAndAgentMethodSimulator (App):
     def build(self):
         Window.clearcolor = (218 / 255, 222 / 255, 206 / 255, 1.0)
 
         return MyScreenManager()
 
 
-CPMapp().run()
+CpmAndAgentMethodSimulator().run()
