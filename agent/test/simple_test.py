ee8b064087f27dea4c5aabb7382cd3d9dbfc2ee0
Nielotz <mati2806@poczta.fm>
2023-05-17 19:40:32 +0200

    Add simple tests

---
+++ agent/test/simple_test.py
@@ -0,0 +1,29 @@
+import pytest
+
+from agent.data_loader import load_data_from_json_file
+
+from agent.agent import AgentData, Agent
+from agent.supply_chain import SupplyChainData, Trader, Route
+
+def runner(path: str):
+    supply_chain_data: SupplyChainData
+    test_ad: AgentData
+    supply_chain_data, test_ad = load_data_from_json_file(path=path)
+
+    result_ad: AgentData = Agent.solve(supply_chain_data=supply_chain_data)
+
+    assert test_ad.total_cost == result_ad.total_cost
+    assert test_ad.total_income == result_ad.total_income
+    assert test_ad.total_profit == result_ad.total_profit
+
+    for t, r in zip(test_ad.profit_table, result_ad.profit_table):
+        assert t == pytest.approx(r)
+
+    for t, r in zip(test_ad.optimal_transport_table, result_ad.optimal_transport_table):
+        assert t == pytest.approx(r)
+
+def test_2x2():
+    runner("agent/test_data/simple_test_2x2.json")
+
+def test_3x3():
+    runner("agent/test_data/simple_test_3x3.json")
