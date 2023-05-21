ab744da2a46ddb6351b286f76cb7642db9fb4179
Nielotz <mati2806@poczta.fm>
2023-05-21 21:19:16 +0200

    Fix and extend agent

--- agent/agent.py
+++ agent/agent.py
@@ -1,18 +1,19 @@
 from dataclasses import dataclass
 
-from agent.supply_chain import SupplyChainData, RoutesTable, TransportTable, Table
+from agent.supply_chain import SupplyChainData, RoutesTable, TransportTable, Table, Route, Trader, FictionalTrader, \
+    FictionalRoute
 
 
 @dataclass
 class AgentData:
-    # total_products_cost: int
-    # total_transport_cost: int
+    total_products_cost: int
+    total_transport_cost: int
     total_cost: int
     total_income: int
     total_profit: int
     buyers: [str]  # Headers in tables.
     sellers: [str]  # Headers in tables.
-    profit_table: [[int, ]]
+    unit_profit_table: [[int, ]]
     optimal_transport_table: [[int, ]]
 
 
@@ -27,30 +28,90 @@ class Agent:
     R [      ]
     S [      ]`"""
 
-    @classmethod
-    def solve(cls, supply_chain_data: SupplyChainData) -> [AgentData, ]:
-        routes_table: RoutesTable = RoutesTable.from_flat(
-            elements=supply_chain_data.routes,
-            dimensions=(len(supply_chain_data.buyers),
-                        len(supply_chain_data.sellers))
+    @staticmethod
+    def convert_to_agent_data(transport_table: TransportTable,
+                              buyers: [Trader],
+                              sellers: [Trader],
+                              routes_table: RoutesTable) -> AgentData:
+        total_products_cost = 0
+        total_transport_cost = 0
+        total_income = 0
+        for row_idx, (transport_table_row, routes_table_row) in enumerate(zip(transport_table, routes_table)):
+            for col_idx, (transported_amount, route) in enumerate(zip(transport_table_row, routes_table_row)):
+                route: Route
+                transported_amount: int
+
+                products_cost = route.seller.price * transported_amount
+                transport_cost = route.transport_cost * transported_amount
+                income = route.buyer.price * transported_amount
+
+                total_products_cost += products_cost
+                total_transport_cost += transport_cost
+                total_income += income
+
+        return AgentData(
+            total_products_cost=total_products_cost,
+            total_transport_cost=total_transport_cost,
+            total_cost=total_products_cost + total_transport_cost,
+            total_income=total_income,
+            total_profit=total_income - (total_products_cost + total_transport_cost),
+            buyers=buyers,
+            sellers=sellers,
+            unit_profit_table=routes_table.unit_profit_table,
+            optimal_transport_table=transport_table.table
         )
 
-        transport_table: TransportTable = TransportTable.create_using_north_west_method(
-            supply_chain_data=supply_chain_data)
+    @classmethod
+    def solve(cls, supply_chain_data: SupplyChainData) -> [AgentData, ]:
+        buyers: [Trader, ] = supply_chain_data.buyers
+        sellers: [Trader, ] = supply_chain_data.sellers
+        routes: [Route] = supply_chain_data.routes
+
+        routes_table: RoutesTable = RoutesTable.from_flat(elements=routes, dimensions=(len(sellers), len(buyers)))
+
+        buyers_capacity = sum([cap.capacity for cap in buyers])
+        sellers_capacity = sum([cap.capacity for cap in sellers])
+
+        if supply_chain_data.force_fictional or buyers_capacity != sellers_capacity:
+            fictional_seller: FictionalTrader = FictionalTrader(capacity=buyers_capacity)
+            sellers.append(fictional_seller)
+            routes_table.add_row(
+                [FictionalRoute(seller=fictional_seller, buyer=buyer, transport_cost=0) for buyer in buyers])
+
+            fictional_buyer: FictionalTrader = FictionalTrader(capacity=sellers_capacity)
+            buyers.append(fictional_buyer)
+            routes_table.add_column(
+                [FictionalRoute(seller=seller, buyer=fictional_buyer, transport_cost=0) for seller in sellers])
+
+        transport_table: TransportTable = TransportTable.create_using_north_west_method(buyers=buyers, sellers=sellers)
 
         transport_table.print(header="Transport table:")
 
         transport_table.init_equations(routes_table=routes_table)
         transport_table.optimality_indicators_equations.print(header="Equations")
 
-        for loops_left in range(20, 0, -1):  # Infinite loop guard.
+        transport_tables: [TransportTable] = []
+
+        for loops_left in range(30, 0, -1):  # Infinite loop guard.
             optimality_indicators: [int] = transport_table.get_optimality_indicators()
             Table(table=[optimality_indicators]).print(header="Optimality indicators")
 
             if min(optimality_indicators) < 0:
                 transport_table.shuffle_products(optimality_indicators=optimality_indicators)
                 transport_table.print(header="Transport table")
+
+                if transport_table in transport_tables:
+                    transport_tables = transport_tables[transport_tables.index(transport_table):]
+                    break
+                else:
+                    transport_tables.append(TransportTable(table=transport_table.table))
             else:
+                transport_tables = [transport_table]
                 break
         else:
             raise RecursionError()
+
+        return [Agent.convert_to_agent_data(transport_table=transport_table,
+                                            buyers=buyers,
+                                            sellers=sellers,
+                                            routes_table=routes_table) for transport_table in transport_tables]
--- agent/data_loader.py
+++ agent/data_loader.py
@@ -21,15 +21,35 @@ def load_data_from_json_file(path: str = "") -> (SupplyChainData, Optional[Agent
         for buyer, route_cost in zip(buyers, road):
             routes.append(Route(seller=seller, buyer=buyer, transport_cost=route_cost))
 
+    force_fictional: bool = supply_chain["force fictional"]
+
     result: AgentData = None
     if "output" in supply_chain_data:
         output = supply_chain_data["output"]
-        result = AgentData(total_cost=output["total cost"],
+        result = AgentData(total_products_cost=output["total products cost"],
+                           total_transport_cost=output["total transport cost"],
+                           total_cost=output["total cost"],
                            total_income=output["total income"],
                            total_profit=output["total profit"],
                            buyers=buyers,
                            sellers=sellers,
-                           profit_table=output["profit table"],
+                           unit_profit_table=output["unit profit table"],
                            optimal_transport_table=output["optimal transport table"])
 
-    return SupplyChainData(sellers=sellers, buyers=buyers, routes=routes), result
+
+    return SupplyChainData(sellers=sellers, buyers=buyers, routes=routes, force_fictional=force_fictional), result
+
+
+def load_data_from_gui(supply: [], demand: [], sell_price: [], buy_price: [], transport_table: [[]],
+                       force_fictional: bool) -> SupplyChainData:
+
+    sellers: [Trader, ] = [Trader(capacity=supply_, price=buy_price_) for supply_, buy_price_ in zip(supply, buy_price)]
+    buyers: [Trader, ] = [Trader(capacity=supply_, price=buy_price_) for supply_, buy_price_ in zip(demand, sell_price)]
+
+    routes: [Route, ] = []
+    sellers_iter = iter(sellers)
+    buyers_iter = iter(buyers)
+    for row in transport_table:
+        routes.append([Route(seller=next(sellers_iter), buyer=next(buyers_iter), transport_cost=cost) for cost in row])
+
+    return SupplyChainData(sellers=sellers, buyers=buyers, routes=routes, force_fictional=force_fictional)
