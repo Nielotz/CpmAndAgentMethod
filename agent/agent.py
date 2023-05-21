0e090918a8876290134a3fcbba8a8e8248c3808a
Nielotz <mati2806@poczta.fm>
2023-05-21 16:00:40 +0200

    Add Solver (#23)
    * Create agent_dev_main.py
    
    * Add solver
    
    * Code cleanup

--- agent/agent.py
+++ agent/agent.py
@@ -1,12 +1,12 @@
 from dataclasses import dataclass
 
-from agent.supply_chain import SupplyChainData
+from agent.supply_chain import SupplyChainData, RoutesTable, TransportTable, Table
 
 
 @dataclass
 class AgentData:
-    total_products_cost: float
-    total_transport_cost: float
+    # total_products_cost: float
+    # total_transport_cost: float
     total_cost: float
     total_income: float
     total_profit: float
@@ -17,6 +17,40 @@ class AgentData:
 
 
 class Agent:
+    """
+`       BUYERS
+    S [      ]
+    E [      ]
+    L [      ]
+    L [      ]
+    E [      ]
+    R [      ]
+    S [      ]`"""
+
     @classmethod
     def solve(cls, supply_chain_data: SupplyChainData) -> [AgentData, ]:
-        pass
+        routes_table: RoutesTable = RoutesTable.from_flat(
+            elements=supply_chain_data.routes,
+            dimensions=(len(supply_chain_data.buyers),
+                        len(supply_chain_data.sellers))
+        )
+
+        transport_table: TransportTable = TransportTable.create_using_north_west_method(
+            supply_chain_data=supply_chain_data)
+
+        transport_table.print(header="Transport table:")
+
+        transport_table.init_equations(routes_table=routes_table)
+        transport_table.optimality_indicators_equations.print(header="Equations")
+
+        for loops_left in range(20, 0, -1):  # Infinite loop guard.
+            optimality_indicators: [int] = transport_table.get_optimality_indicators()
+            Table(table=[optimality_indicators]).print(header="Optimality indicators")
+
+            if min(optimality_indicators) < 0:
+                transport_table.shuffle_products(optimality_indicators=optimality_indicators)
+                transport_table.print(header="Transport table")
+            else:
+                break
+        else:
+            raise RecursionError()
